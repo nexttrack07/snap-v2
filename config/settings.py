4b9520dbf84156a19c047ae8ad47d6934290ddbc
Faadhil Hakim <faadhil@nexttrack.biz>
2023-02-18 21:25:02 -0600

    change static root

--- config/settings.py
+++ config/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'theme/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
